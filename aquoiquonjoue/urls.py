f36b5e1c857de688a1cff9f8f3c318f31e23a825
Malfeisoif <clement.pivot@protonmail.com>
2020-02-20 09:59:51 +0100

    feat: robots.txt

--- aquoiquonjoue/urls.py
+++ aquoiquonjoue/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.http import HttpResponse
 
 urlpatterns = [
     path('', include('jeux.urls')),
     path('rectangle_game/', include('rectangle_game.urls')),
     path('chat/', include('chat.urls')),
     path('admin/', admin.site.urls),
+    re_path(r'^robots.txt$', lambda r: HttpResponse("User-agent: *</br>Disallow: /")),
 ]
