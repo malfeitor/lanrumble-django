4ba9bf486d6305baeb7014cc15853accd9901809
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-06-28 17:00:36 +0200

    feat: create API app ✨

---
+++ api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework_simplejwt import views as jwt_views
+from . import views
+
+app_name = "api"
+urlpatterns = [
+    path("home/", views.HomeView.as_view(), name="home"),
+    path("token/", jwt_views.TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", jwt_views.TokenRefreshView.as_view(), name="token_refresh"),
+    path("api-auth/", include("rest_framework.urls")),
+]
