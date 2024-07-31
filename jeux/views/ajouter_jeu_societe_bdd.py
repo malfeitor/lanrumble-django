e9f7146a6172792eb741220dd19870a8e22633b2
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-07-31 15:29:54 +0200

    feat: guest cant modify BDD ✨

--- jeux/views/ajouter_jeu_societe_bdd.py
+++ jeux/views/ajouter_jeu_societe_bdd.py
@@ -7,31 +7,35 @@ from datetime import time
 
 @login_required(login_url='/')
 def ajouter_jeu_societe_bdd(request):
-    if (int(request.POST.get('joueurs_min')) >= 1 and int(request.POST.get('joueurs_max')) >= 2 and
-        len(request.POST.get('nom')) >= 4 and len(request.POST.get('nom')) < 50 and
-            (int(request.POST.get('temps_prevu_h')) >= 0 and int(request.POST.get('temps_prevu_min')) >= 0)):
-        for jeu in Jeu_Societe.objects.all():
-            if(jeu.nom.lower() == request.POST.get('nom').lower()):
-                print(jeu.nom + " existe déjà.")
-                request.session['error_not_seen'] = True
-                request.session['error_message'] = "Erreur : " + \
-                    jeu.nom + " existe déjà.\\n"
-                return redirect('jeux:jeux_societe')
-        jeu_ajouter = Jeu_Societe.objects.create(
-            nom=str(request.POST.get('nom')).title(),
-            complexite=int(request.POST.get('complexite')),
-            temps_prevu=time(int(request.POST.get('temps_prevu_h')), int(
-                request.POST.get('temps_prevu_min'))),
-            joueurs_min=int(request.POST.get('joueurs_min')),
-            joueurs_max=int(request.POST.get('joueurs_max')))
-        jeu_ajouter.complexite = jeu_ajouter.complexite if jeu_ajouter.complexite < 6 and jeu_ajouter.complexite > 0 else 1
-        for pseudo in request.POST.getlist('possesseurs'):
-            jeu_ajouter.possede_par.add(Joueur.objects.get(
-                utilisateur=User.objects.get(username=pseudo)))
-        jeu_ajouter.save()
-        print(jeu_ajouter.nom +
-              " correctement enregistré avec l'id " + str(jeu_ajouter.id))
-    else:
+    if request.user.groups.filter(name = 'guest').exists():
         request.session['error_not_seen'] = True
-        request.session['error_message'] += "Erreur dans les informations du jeu.\\n"
+        request.session['error_message'] += "Vous n'avez pas l'autorisation de créer un jeu.\\n"
+    else:
+        if (int(request.POST.get('joueurs_min')) >= 1 and int(request.POST.get('joueurs_max')) >= 2 and
+            len(request.POST.get('nom')) >= 4 and len(request.POST.get('nom')) < 50 and
+                (int(request.POST.get('temps_prevu_h')) >= 0 and int(request.POST.get('temps_prevu_min')) >= 0)):
+            for jeu in Jeu_Societe.objects.all():
+                if(jeu.nom.lower() == request.POST.get('nom').lower()):
+                    print(jeu.nom + " existe déjà.")
+                    request.session['error_not_seen'] = True
+                    request.session['error_message'] = "Erreur : " + \
+                        jeu.nom + " existe déjà.\\n"
+                    return redirect('jeux:jeux_societe')
+            jeu_ajouter = Jeu_Societe.objects.create(
+                nom=str(request.POST.get('nom')).title(),
+                complexite=int(request.POST.get('complexite')),
+                temps_prevu=time(int(request.POST.get('temps_prevu_h')), int(
+                    request.POST.get('temps_prevu_min'))),
+                joueurs_min=int(request.POST.get('joueurs_min')),
+                joueurs_max=int(request.POST.get('joueurs_max')))
+            jeu_ajouter.complexite = jeu_ajouter.complexite if jeu_ajouter.complexite < 6 and jeu_ajouter.complexite > 0 else 1
+            for pseudo in request.POST.getlist('possesseurs'):
+                jeu_ajouter.possede_par.add(Joueur.objects.get(
+                    utilisateur=User.objects.get(username=pseudo)))
+            jeu_ajouter.save()
+            print(jeu_ajouter.nom +
+                " correctement enregistré avec l'id " + str(jeu_ajouter.id))
+        else:
+            request.session['error_not_seen'] = True
+            request.session['error_message'] += "Erreur dans les informations du jeu.\\n"
     return redirect('jeux:jeux_societe')
--- jeux/views/api.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from ..serializers import JeuSerializer
-from ..models import Jeu
-
-class JeuxView(viewsets.ModelViewSet):
-	serializer_class = JeuSerializer
-	queryset = Jeu.objects.all()
