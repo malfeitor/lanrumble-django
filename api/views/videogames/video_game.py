a7bc56426d600e6b54b45cf51eede9d2e0617ac7
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-10-16 19:11:39 +0200

    feat: create videogame api endpoint ✨

---
+++ api/views/videogames/video_game.py
@@ -0,0 +1,15 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from jeux.models import Jeu
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
+
+
+class VideogameView(APIView):
+    def get(self, request, id):
+        try:
+            game = Jeu.objects.get(pk=id)
+            print(game.nom)
+        except ObjectDoesNotExist:
+            raise Http404
+        return Response(game.nom)
