7056eba20565ec53655f85547f6305675ece6c66
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-10-16 20:24:05 +0200

    style: remove hyphen in videogames 💎

--- api/urls.py
+++ api/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 app_name = "api"
 urlpatterns = [
-    path("video-games", views.VideogamesView.as_view(), name="video_games"),
-    path("video-game/<id>", views.VideogameView.as_view(), name="video_game"),
-    path("user/<userId>/video-games", views.UserVideogamesView.as_view(), name="video_games"),
+    path("videogames", views.VideogamesView.as_view(), name="video_games"),
+    path("videogame/<id>", views.VideogameView.as_view(), name="video_game"),
+    path("user/<user_id>/videogames", views.UserVideogamesView.as_view(), name="video_games"),
     path("token/", jwt_views.TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", jwt_views.TokenRefreshView.as_view(), name="token_refresh"),
     path("api-auth/", include("rest_framework.urls")),
