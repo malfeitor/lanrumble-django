393ae3dd9a22e02dedd685b408ad59139dcc8a8a
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-10-17 11:30:52 +0200

    test: create anonymous and logged setup 🚨

--- api/tests/test_setup.py
+++ api/tests/test_setup.py
@@ -1,15 +1,43 @@
 from rest_framework.test import APITestCase, APIClient
 from django.contrib.auth.models import User
 from jeux.models import Joueur
+import json
 
 
-class TestSetup(APITestCase):
+class TestSetupAnonymous(APITestCase):
     def setUp(self):
         self.client = APIClient()
-        self.user = User.objects.create_user(username='user', email='user@foo.com', password='pass')
+        self.user = User.objects.create_user(
+            username="user", email="user@foo.com", password="pass"
+        )
         self.user.save()
         self.player = Joueur.objects.create(utilisateur=self.user)
         self.player.save()
+
+        return super().setUp()
+
+    def tearDown(self):
+        return super().tearDown()
+
+
+class TestSetupLoged(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(
+            username="user", email="user@foo.com", password="pass"
+        )
+        self.user.save()
+        self.player = Joueur.objects.create(utilisateur=self.user)
+        self.player.save()
+        login_data = {"username": "user", "password": "pass"}
+        request = self.client.post(
+            "/api/token/",
+            json.dumps(login_data),
+            content_type="application/json",
+            secure=True,
+        )
+        self.accessToken = request.data["access"]
+        self.refreshToken = request.data["refresh"]
 
         return super().setUp()
 
--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -1,11 +1,14 @@
-from .test_setup import TestSetup
+from .test_setup import TestSetupAnonymous
 import json
 
-class TestViews(TestSetup):
+
+class TestViewsAnonymous(TestSetupAnonymous):
     def test_joueur_login_successfull(self):
-        login_data = { 'username': 'user', 'password': 'pass'}
-        request = self.client.post('/api/token/',
-                                    json.dumps(login_data),
-                                    content_type='application/json',
-                                    secure=True)
+        login_data = {"username": "user", "password": "pass"}
+        request = self.client.post(
+            "/api/token/",
+            json.dumps(login_data),
+            content_type="application/json",
+            secure=True,
+        )
         self.assertEqual(request.status_code, 200)
