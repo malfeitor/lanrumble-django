81c7748a32f0923b6de0ae1b93d170435ffe113b
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-11-05 10:47:11 +0100

    test: create test user for logged tests 🚨

--- api/tests/test_setup.py
+++ api/tests/test_setup.py
@@ -7,13 +7,7 @@ import json
 class TestSetupAnonymous(APITestCase):
     def setUp(self):
         self.client = APIClient()
-        self.user = User.objects.create_user(
-            username="user", email="user@foo.com", password="pass"
-        )
-        self.user.save()
-        self.player = Player.objects.create(utilisateur=self.user)
-        self.player.save()
-
+        create_user(self)
         return super().setUp()
 
     def tearDown(self):
@@ -23,53 +17,50 @@ class TestSetupAnonymous(APITestCase):
 class TestSetupLoged(APITestCase):
     def setUp(self):
         self.client = APIClient()
-        self.user = User.objects.create_user(
-            username="user", email="user@foo.com", password="pass"
-        )
-        self.user.save()
-        self.player = Player.objects.create(utilisateur=self.user)
-        self.player.save()
-        login_data = {"username": "user", "password": "pass"}
-        request = self.client.post(
-            "/api/token/",
-            json.dumps(login_data),
-            content_type="application/json",
-            secure=True,
-        )
-        self.accessToken = request.data["access"]
-        self.refreshToken = request.data["refresh"]
-        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + self.accessToken)
-
+        create_user(self)
+        authentificate(self)
         return super().setUp()
 
     def tearDown(self):
         return super().tearDown()
 
 
-class TestSetupWithGames(APITestCase):
-    def setUp(self):
-        self.client = APIClient()
-        self.user = User.objects.create_user(
-            username="user", email="user@foo.com", password="pass"
-        )
-        self.user.save()
-        self.player = Player.objects.create(utilisateur=self.user)
-        self.player.save()
-        login_data = {"username": "user", "password": "pass"}
-        request = self.client.post(
-            "/api/token/",
-            json.dumps(login_data),
-            content_type="application/json",
-            secure=True,
-        )
-        self.accessToken = request.data["access"]
-        self.refreshToken = request.data["refresh"]
-        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + self.accessToken)
+def authentificate(self):
+    login_data = {"username": "user", "password": "pass"}
+    request = self.client.post(
+        "/api/token/",
+        json.dumps(login_data),
+        content_type="application/json",
+        secure=True,
+    )
+    self.accessToken = request.data["access"]
+    self.refreshToken = request.data["refresh"]
+    self.client.credentials(HTTP_AUTHORIZATION="Bearer " + self.accessToken)
 
-        videogame = Videogame.objects.create()
-        videogame.save()
 
-        return super().setUp()
+def create_user(self):
+    self.user = User.objects.create_user(
+        username="user", email="user@foo.com", password="pass"
+    )
+    self.user.save()
+    self.player = Player.objects.create(user=self.user)
+    self.player.save()
 
-    def tearDown(self):
-        return super().tearDown()
+
+def create_game(
+    title,
+    coop,
+    pvp,
+    max_hot_seat_players,
+    max_online_players,
+    f2p,
+):
+    videogame = Videogame.objects.create(
+        title=title,
+        coop=coop,
+        pvp=pvp,
+        max_hot_seat_players=max_hot_seat_players,
+        max_online_players=max_online_players,
+        f2p=f2p,
+    )
+    videogame.save()
