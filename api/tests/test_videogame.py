6c69a05f2af7fe6b5cb227de63456fa047631978
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-11-05 10:47:39 +0100

    test: create a game before test it 🚨

--- api/tests/test_videogame.py
+++ api/tests/test_videogame.py
@@ -1,7 +1,19 @@
-from .test_setup import TestSetupLoged, TestSetupAnonymous
+from .test_setup import TestSetupLoged, TestSetupAnonymous, create_game
+import logging
 
 
 class TestViewLogged(TestSetupLoged):
+    @classmethod
+    def setUpTestData(cls):
+        create_game(
+            title="Helldivers 2",
+            coop=True,
+            pvp=False,
+            max_hot_seat_players=1,
+            max_online_players=4,
+            f2p=False,
+        )
+
     def test_get_game_1(self):
         request = self.client.get(
             "/api/videogame/1",
